eb8d504783ee1fa91cb1a91110cbeedbf2e8a2b6
Albin Kjellson <albinkj91@yahoo.com>
2025-01-03 08:18:24 +0100

    Figuring out data structures and adding logic for fall animation

--- index.js
+++ index.js
@@ -4,9 +4,18 @@ const canvasHeight = ctx.height;
 const fieldOffsetX = 350;
 const fieldOffsetY = 100;
 
-class Field{
+const Colors = Object.freeze({
+	RED: 1,
+	BLUE: 2,
+	GREEN: 3,
+	YELLOW: 4,
+	TEAL: 5,
+	PURPLE: 6
+});
+
+class Game{
 	constructor(){
-		this.blocks = [
+		this.field = [
 			[0,0,0,0,0,0,0,0,0,0],
 			[0,0,0,0,0,0,0,0,0,0],
 			[0,0,0,0,0,0,0,0,0,0],
@@ -30,18 +39,65 @@ class Field{
 		];
 	}
 
+	isCollision(tetromino){
+		for(let i = 0; i < tetromino.blocks.length; i++){
+			if(tetromino.blocks[i][j] !== 0){
+				this.field[tetromino.y+i][tetromino.x+j] = tetromino.blocks[i][j];
+			}
+		}
+	}
+
 	place(tetromino){
 		if(tetromino.x > 9 || tetromino.x < 0 || tetromino.y > 19 || tetromino.y < 0)
 			throw new RangeError("Tetromino can't be placed outside field boundary.");
+
+		for(let i = 0; i < tetromino.blocks.length; i++){
+			this.field[tetromino.y+tetromino.blocks[i].y][tetromino.x+tetromino.blocks[i].x] = 1;
+		}
+	}
+
+	remove(tetromino){
+		for(let i = 0; i < tetromino.blocks.length; i++){
+			this.field[tetromino.y+tetromino.blocks[i].y][tetromino.x+tetromino.blocks[i].x] = 0;
+		}
+	}
+
+	renderField(){
+		for(let i = 0; i < this.field.length; i++){
+			for(let j = 0; j < this.field[i].length; j++){
+				switch(this.field[i][j]){
+					case Colors.RED:
+						ctx.fillStyle = "#dd3030";
+						break;
+					case Colors.BLUE:
+						ctx.fillStyle = "#3030ff";
+						break;
+					case Colors.GREEN:
+						ctx.fillStyle = "#30dd30";
+						break;
+					case Colors.YELLOW:
+						ctx.fillStyle = "#aaaa30";
+						break;
+					case Colors.TEAL:
+						ctx.fillStyle = "#90dddd";
+						break;
+					case Colors.PURPLE:
+						ctx.fillStyle = "#bb30bb";
+						break;
+					default:
+						ctx.fillStyle = "black";
+				}
+				ctx.fillRect(fieldOffsetX + j*30, fieldOffsetY + i*30, 28, 28);
+			}
+		}
 	}
 }
 
 class Tetromino{
-	constructor(blocks, x, y, color) {
+	constructor(blocks, x, y) {
 		this.blocks = blocks;
 		this.x = x;
 		this.y = y;
-		this.color = color;
 	}
 
 	rotate(){
@@ -49,52 +105,43 @@ class Tetromino{
 	}
 }
 
-const drawTetromino = (tetromino) =>{
-	//TODO:
-}
-
-const renderField = (field) =>{
-	for(let i = 0; i < field.blocks.length; i++){
-		for(let j = 0; j < field.blocks[i].length; j++){
-			switch(field.blocks[i][j]){
-				case 0:
-					ctx.fillStyle = "#90ffff";
-					ctx.fillRect(fieldOffsetX + j*30, fieldOffsetY + i*30, 28, 28);
-					break;
-				default:
-					throw new Error("Invalid value on field");
-			}
-		}
+class Vec2{
+	constructor(x, y){
+		this.x = x;
+		this.y = y;
 	}
-};
-
-const randomizeTetromino = () =>{
-	//TODO:
 }
 
 const update = () =>{
+	previousState = new Tetromino(currentTetromino.blocks, currentTetromino.x, currentTetromino.y);
 	currentTetromino.y += 1;
-	field.place(currentTetromino);
+	game.remove(previousState);
+	game.isCollision(currentTetromino);
+	game.place(currentTetromino);
 }
 
+let start = 0;
 const step = (timestamp) =>{
-	const elapsed = timestamp % 1000;
-	if(elapsed > 990){
+	const elapsed = timestamp - start;
+	if(elapsed > 1000){
 		update();
 		ctx.clearRect(0, 0, canvasWidth, canvasHeight);
-		renderField();
+		game.renderField();
+		start = timestamp;
 	}
 	requestAnimationFrame(step);
 }
 
 // *********************** START ************************
-const squareBlocks = [[1, 1],
-					  [1, 1]];
+const squareBlocks = [new Vec2(0, 0), new Vec2(1, 0), new Vec2(0, 1), new Vec2(1, 1)];
+//const straightBlocks = [1, 1, 1, 1];
 
-const square = new Tetromino(squareBlocks, 4, 19);
+const square = new Tetromino(squareBlocks, 4, 0);
 let currentTetromino = square;
-const field = new Field();
+let previousState = square;
+const game = new Game();
 
-renderField(field);
-field.place(square);
+game.renderField();
+game.place(square);
+game.renderField();
 //requestAnimationFrame(step);
